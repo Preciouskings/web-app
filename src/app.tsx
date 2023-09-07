2417ff2de87659680965856d708627ded5e87d7e
Adophilus <uchenna19of@gmail.com>
2023-09-07 12:43:11 +0100

    updated the view

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
+import HomeeaseApi from "@homeease/api";
+import { Zodios } from "@zodios/core";
 
 const App: Component = () => {
-  return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-  );
+	const client = new Zodios('http://localhost:5000/api', HomeeaseApi);
+	console.log(client)
+
+	return (
+		<p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+	);
 };
 
 export default App;
