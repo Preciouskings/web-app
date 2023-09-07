ff4118dc5bc086cf0cca399beeb417d1dedf3934
Adophilus <uchenna19of@gmail.com>
2023-09-07 12:36:13 +0100

    scaffolded a solid app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-  './components/**/*.{html,js}',
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}"
   ],
   theme: {
     extend: {},
